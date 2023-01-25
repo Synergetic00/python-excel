4bb7fd68a4b94a926d5e35aa1120f5d2339ff47d
Synergetic00 <synergetic00@gmail.com>
2023-01-25 13:04:16 +1100

    Added docstring to module

--- pyxl/functions.py
+++ pyxl/functions.py
@@ -1,4 +1,6 @@
-def ABS(number: str | int):
+'''Excel function in Python'''
+
+def ABS(number: str | int) -> int:
     '''Returns the absolute value of a number'''
     try: number = int(str(number))
     except: raise TypeError('Only integers are allowed')
